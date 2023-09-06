b9d55efc4f79b4d46e0feff78bdec78d13c2f14e
yunjichoi9151 <cyjcode99@gmail.com>
2023-09-06 23:45:18 +0900

    🚧 Progress: Main Page Update
    1. Main Page에 lottie file 추가해서 임시 Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import { useLottie } from 'lottie-react';
+import spaceDeveloper from '../public/Animation/space_developer.json';
 
 const App = () => {
+  const options = {
+    animationData: spaceDeveloper,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
+
   return (
-    <>
-      <div>Bluemoon</div>
-    </>
+    <div>
+      <div
+        style={{
+          width: '100%',
+          display: 'grid',
+          justifyContent: 'center',
+        }}
+      >
+        {View}
+      </div>
+      <div
+        style={{
+          textAlign: 'center',
+          paddingLeft: '15px',
+          fontSize: '30px',
+          fontWeight: '1000',
+        }}
+      >
+        Bluemoon Project
+      </div>
+    </div>
   );
 };
 
