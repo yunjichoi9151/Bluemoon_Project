70394b5c195a46dd792b309ec24d32c81f43aa12
yunjichoi9151 <cyjcode99@gmail.com>
2023-12-06 10:18:58 +0900

    ♻️ Refactor: webpack 설정 리팩토링
    1. ES6 모듈 문법으로 리팩토링

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import path from 'path';
 
-module.exports = {
+export default {
   entry: './src/index.tsx',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
@@ -14,21 +14,19 @@ module.exports = {
         use: ['ts-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
       },
     ],
   },
-
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(process.cwd(), '/dist'),
     filename: 'bundle.js',
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+import { merge } from 'webpack-merge';
+import common from './webpack.common.js';
 
-module.exports = merge(common, {
+export default merge(common, {
   mode: 'development',
   devtool: 'eval',
   devServer: {
@@ -9,4 +9,5 @@ module.exports = merge(common, {
     port: 3000,
     hot: true,
   },
+  output: { publicPath: '/' },
 });
