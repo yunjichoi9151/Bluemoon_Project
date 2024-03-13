5dedf0d459ff2dce9b86c2ae42c13cff41e46341
yunjichoi9151 <cyjcode99@gmail.com>
2024-03-13 23:41:11 +0900

    📝 Rename: config 관련 재설정
    1. import 재설정

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,14 @@
 import { merge } from 'webpack-merge';
-import common from './webpack.common.js';
+import config from './webpack.config.js';
 
-export default merge(common, {
+export default merge(config, {
   mode: 'development',
   devtool: 'eval',
   devServer: {
     historyApiFallback: true,
     port: 3000,
     hot: true,
+    open: true,
   },
   output: { publicPath: '/' },
 });
