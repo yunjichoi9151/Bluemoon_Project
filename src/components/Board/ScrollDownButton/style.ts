a0cc6a56fd4d8f7904dbeaae26178878a0d1936a
yunjichoi9151 <cyjcode99@gmail.com>
2024-03-14 00:46:43 +0900

    📌 Init: ScrollDownButton 생성
    1. ScrollDownButton 생성

---
+++ src/components/Board/ScrollDownButton/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ScrollDownButton = styled.button`
+  position: fixed;
+  width: 125px;
+  bottom: 0px;
+  z-index: 1000;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+`;
