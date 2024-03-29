5c41801388b638607c1535c69ff2ad95593b7eeb
yunjichoi9151 <cyjcode99@gmail.com>
2024-03-29 23:59:33 +0900

    🎯 Fix: useResponsive 수정
    1. useResponsive 수정

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -1,25 +1,37 @@
 import { useMediaQuery } from 'react-responsive';
 
-interface MediaProps {
+const Desktop = ({
+  children,
+}: {
   children: JSX.Element;
-}
-
-const Desktop = ({ children }: MediaProps): JSX.Element | null => {
+}): JSX.Element | null => {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
   return isDesktop ? children : null;
 };
 
-const Tablet = ({ children }: MediaProps): JSX.Element | null => {
+const Tablet = ({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element | null => {
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
   return isTablet ? children : null;
 };
 
-const Mobile = ({ children }: MediaProps): JSX.Element | null => {
+const Mobile = ({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element | null => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   return isMobile ? children : null;
 };
 
-const Default = ({ children }: MediaProps): JSX.Element | null => {
+const Default = ({
+  children,
+}: {
+  children: JSX.Element;
+}): JSX.Element | null => {
   const isNotMobile = useMediaQuery({ minWidth: 768 });
   return isNotMobile ? children : null;
 };
