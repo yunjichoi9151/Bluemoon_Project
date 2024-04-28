7199889559ab1d7d9757664b0ab8d4732c470b2a
yunjichoi9151 <cyjcode99@gmail.com>
2024-04-29 05:53:11 +0900

    🎯 Fix: Main 수정
    1. 전체 내용이 포함되게끔 수정

--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const AllWrap = styled.div`
   width: 100%;
-  height: calc(var(--vh, 1vh) * 100);
+  height: 100%;
+  /* height: calc(var(--vh, 1vh) * 100); */
   display: flex;
   flex-direction: column;
   /* padding-bottom: 200px; */
