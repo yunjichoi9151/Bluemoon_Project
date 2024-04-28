8bdd86f437a2b036cf3fbc4d20e3151855948e0c
yunjichoi9151 <cyjcode99@gmail.com>
2024-04-29 05:44:04 +0900

    🎯 Fix: routes 설정 변경
    1. routes 설정 변경

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,14 +1,25 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '../pages/Main';
 import DetailPage from '../pages/Detail';
+import Layout from '../styles/Layout';
+import UpdatePage from '../pages/Update';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <MainPage />,
-  },
-  {
-    path: '/:project',
-    element: <DetailPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <MainPage />,
+      },
+      {
+        path: 'test',
+        element: <UpdatePage />,
+      },
+      {
+        path: ':project',
+        element: <DetailPage />,
+      },
+    ],
   },
 ]);
