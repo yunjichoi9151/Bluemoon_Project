3653f205082b4b839dc4dbf7a414ecefc48d0592
yunjichoi9151 <cyjcode99@gmail.com>
2024-04-29 05:47:54 +0900

    🎨 Design: BasicModal 디자인 수정
    1. 일부 디자인 옵션 추가, 수정

--- src/components/common/BasicModal/index.tsx
+++ src/components/common/BasicModal/index.tsx
@@ -5,10 +5,11 @@ import { IoCloseSharp } from 'react-icons/io5';
 
 interface ModalProps {
   closeModal: () => void;
+  style?: any;
   children: React.ReactNode;
 }
 
-const BasicModal = ({ closeModal, children }: ModalProps) => {
+const BasicModal = ({ closeModal, style, children }: ModalProps) => {
   return (
     <S.BasicModal
       onClick={(e) => {
@@ -17,7 +18,7 @@ const BasicModal = ({ closeModal, children }: ModalProps) => {
         }
       }}
     >
-      <S.ModalContent>
+      <S.ModalContent style={style}>
         <S.CloseBtn>
           <IconButton handleOnClickButton={closeModal}>
             <IoCloseSharp />
