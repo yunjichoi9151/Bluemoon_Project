5457fc0037614164c16e274cb124719149114b5d
yunjichoi9151 <cyjcode99@gmail.com>
2024-04-29 05:45:37 +0900

    ✨ Feat: Update용 페이지 추가
    1. Update시 활용할 페이지 추가

---
+++ src/pages/Update/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const AllWrap = styled.div`
+  width: 100%;
+  height: calc(var(--vh, 1vh) * 100);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MiddleWrap = styled.div`
+  width: 40%;
+`;
+
+export const Text = styled.div`
+  font-family: Gowun Dodum;
+  padding: 0.5rem;
+  font-weight: bold;
+  font-size: 1rem;
+  color: white;
+`;
