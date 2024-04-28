88c102ca9f266a7e2c1b7f2c8cd70aa06ec646fb
yunjichoi9151 <cyjcode99@gmail.com>
2024-04-29 05:51:21 +0900

    🎯 Fix: About 수정
    1. 필요한 추가 정보들 담아서 수정

--- src/components/Home/About/index.tsx
+++ src/components/Home/About/index.tsx
@@ -2,71 +2,147 @@ import * as S from './style';
 import BasicText from '../../common/BasicText';
 import ME from '../../../assets/img/Me.webm';
 import ScrollAnimation from 'react-animate-on-scroll';
+import Circle3 from '../../../assets/img/circle3.png';
+import { IoPerson } from 'react-icons/io5';
+import { FaGithub } from 'react-icons/fa';
+import { FaPhone, FaInstagram } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
+import { SiTistory } from 'react-icons/si';
 
 const About = () => {
+  const handleEmailClick = () => {
+    window.location.href = 'mailto:example@example.com';
+  };
+
+  const handlePhoneClick = () => {
+    window.location.href = 'tel:+1234567890';
+  };
   return (
     <>
       <S.AllWrap id='about'>
-        <S.TopWrap>
-          <BasicText
-            text='ABOUT ME'
-            color='white'
-            font='Raleway'
-            fontStyle='--font-heading-xxxl'
-          />
-        </S.TopWrap>
-        <S.MiddleWrap>
-          <S.Avatar muted autoPlay loop>
-            <source src={ME} type='video/webm' />
-          </S.Avatar>
-          <S.Content>
-            <BasicText
-              text='안녕하세요. 장인정신을 가진 개발자 최윤지입니다.'
-              color='white'
-              font='Gowun Dodum'
-              fontStyle='--font-paragraph-large'
-            />
-            <S.OneLine>
-              <BasicText
-                text="저는 '"
-                color='white'
-                font='Gowun Dodum'
-                fontStyle='--font-paragraph-large'
-              />
-              <BasicText
-                text='사용자의 입장에서 생각하고, 개발하고, 개선하자'
-                color='#C961DE'
-                font='Gowun Dodum'
-                fontStyle='--font-paragraph-large'
-              />
-              <BasicText
-                text="'라는 마음가짐으로 개발에 임합니다."
-                color='white'
-                font='Gowun Dodum'
-                fontStyle='--font-paragraph-large'
-              />
-            </S.OneLine>
-
-            <BasicText
-              text='서비스를 여러 번 테스트하며 사소한 오류나 불편함을 개선하기 위해 노력합니다.'
-              color='white'
-              font='Gowun Dodum'
-              fontStyle='--font-paragraph-large'
-            />
-            <BasicText
-              text='언제나 더 나은 방향을 모색하고, 새로운 기술에도 즐거운 마음으로 도전합니다.'
-              color='white'
-              font='Gowun Dodum'
-              fontStyle='--font-paragraph-large'
-            />
-            <BasicText
-              text='높은 재사용성을 가진 컴포넌트 설계와, 클린 코드 작성을 목표로 두고 있습니다.'
-              color='white'
-              font='Gowun Dodum'
-              fontStyle='--font-paragraph-large'
-            />
-          </S.Content>
-        </S.MiddleWrap>
+        <S.TopWrap>ABOUT ME</S.TopWrap>
+        <S.MiddleContent>
+          <S.IntroAllWrap>
+            <S.IntroWrap>
+              <S.OneIntroWrap>
+                <S.IntroIcon>
+                  <IoPerson color='white' size='3rem' />
+                </S.IntroIcon>
+                <S.IntroTextWrap>
+                  <S.IntroCategory>Name</S.IntroCategory>
+                  <S.IntroContent>최윤지</S.IntroContent>
+                </S.IntroTextWrap>
+              </S.OneIntroWrap>
+              <S.OneIntroWrap>
+                <S.IntroIcon>
+                  <MdEmail color='white' size='3rem' />
+                </S.IntroIcon>
+                <S.IntroTextWrap>
+                  <S.IntroCategory>Email</S.IntroCategory>
+                  <S.IntroContent onClick={handleEmailClick}>
+                    cyjcode99@gmail.com
+                  </S.IntroContent>
+                </S.IntroTextWrap>
+              </S.OneIntroWrap>
+              <S.OneIntroWrap>
+                <S.IntroIcon>
+                  <FaPhone color='white' size='3rem' />
+                </S.IntroIcon>
+                <S.IntroTextWrap>
+                  <S.IntroCategory>Phone</S.IntroCategory>
+                  <S.IntroContent onClick={handlePhoneClick}>
+                    010-6831-9152
+                  </S.IntroContent>
+                </S.IntroTextWrap>
+              </S.OneIntroWrap>
+            </S.IntroWrap>
+            <S.IntroWrap>
+              <S.OneIntroWrap>
+                <S.IntroIcon>
+                  <FaGithub color='white' size='3rem' />
+                </S.IntroIcon>
+                <S.IntroTextWrap>
+                  <S.IntroCategory>GitHub</S.IntroCategory>
+                  <S.IntroContent
+                    onClick={() =>
+                      window.open('https://github.com/yunjichoi9151/', '_blank')
+                    }
+                  >
+                    yunjichoi9151
+                  </S.IntroContent>
+                </S.IntroTextWrap>
+              </S.OneIntroWrap>
+              <S.OneIntroWrap>
+                <S.IntroIcon>
+                  <SiTistory color='white' size='3rem' />
+                </S.IntroIcon>
+                <S.IntroTextWrap>
+                  <S.IntroCategory>Blog</S.IntroCategory>
+                  <S.IntroContent
+                    onClick={() =>
+                      window.open('https://bluemoon99.tistory.com/', '_blank')
+                    }
+                  >
+                    bluemoon99.tistory.com
+                  </S.IntroContent>
+                </S.IntroTextWrap>
+              </S.OneIntroWrap>
+              <S.OneIntroWrap>
+                <S.IntroIcon>
+                  <FaInstagram color='white' size='3rem' />
+                </S.IntroIcon>
+                <S.IntroTextWrap>
+                  <S.IntroCategory>Instagram</S.IntroCategory>
+                  <S.IntroContent
+                    onClick={() =>
+                      window.open(
+                        'https://www.instagram.com/bluemoon_cyj/',
+                        '_blank'
+                      )
+                    }
+                  >
+                    bluemoon_cyj
+                  </S.IntroContent>
+                </S.IntroTextWrap>
+              </S.OneIntroWrap>
+            </S.IntroWrap>
+          </S.IntroAllWrap>
+          <S.MiddleWrap>
+            <S.Avatar muted autoPlay loop>
+              <source src={ME} type='video/webm' />
+            </S.Avatar>
+            <S.Content>
+              <S.ContentP>
+                안녕하세요.{' '}
+                <S.ContentSpan>장인정신을 가진 개발자 </S.ContentSpan>
+                최윤지입니다.
+              </S.ContentP>
+              <S.ContentP>
+                저는 '
+                <S.ContentSpan>
+                  사용자의 입장에서 생각하고, 개발하고, 개선하자
+                </S.ContentSpan>
+                '라는 마음가짐으로 개발에 임합니다.
+              </S.ContentP>
+              <S.ContentP>
+                서비스를 여러 번 <S.ContentSpan>테스트</S.ContentSpan>하며
+                사소한 오류나 불편함을 개선하기 위해 노력합니다.
+              </S.ContentP>
+              <S.ContentP>
+                언제나 더 나은 방향을 모색하고, 새로운 기술에도 즐거운 마음으로
+                <S.ContentSpan> 도전</S.ContentSpan>합니다.
+              </S.ContentP>
+              <S.ContentP>
+                <S.ContentSpan>
+                  높은 재사용성을 가진 컴포넌트 설계
+                </S.ContentSpan>
+                와, <S.ContentSpan>클린 코드 작성</S.ContentSpan>을 목표로 두고
+                있습니다.
+              </S.ContentP>
+            </S.Content>
+          </S.MiddleWrap>
+        </S.MiddleContent>
+        <S.Circle3 src={Circle3} />
       </S.AllWrap>
     </>
   );
--- src/components/Home/About/style.ts
+++ src/components/Home/About/style.ts
@@ -2,26 +2,53 @@ import styled from 'styled-components';
 
 export const AllWrap = styled.div`
   width: 100%;
-  padding: 0 calc(var(--vw, 1vw) * 20);
   height: calc(var(--vh, 1vh) * 100);
+  padding: 5rem 3rem;
   display: flex;
   flex-direction: column;
 `;
 
+export const MiddleContent = styled.div`
+  z-index: 999;
+  margin: auto;
+  padding: 2rem;
+  /* width: 100%; */
+  @media screen and (max-width: 87.5rem) {
+    max-width: 82.5rem;
+  }
+  @media screen and (max-width: 75rem) {
+    max-width: 71.25rem;
+  }
+  @media screen and (max-width: 62rem) {
+    max-width: 60rem;
+  }
+  @media screen and (max-width: 48rem) {
+    max-width: 45rem;
+  }
+  @media screen and (max-width: 36rem) {
+    max-width: 33.75rem;
+  }
+`;
+
 export const TopWrap = styled.div`
-  height: 50px;
-  margin-top: 8rem;
-  /* padding: 0rem 5rem; */
+  /* height: 50px; */
+  /* margin-top: 8rem; */
+  padding: 1rem 0rem;
+  font: var(--font-heading-xxl) Raleway;
+  color: white;
+  text-align: center;
 `;
 
 export const MiddleWrap = styled.div`
-  height: calc(var(--vh, 1vh) * 50);
+  /* height: 35rem; */
   /* padding: 100px; */
   justify-content: space-between;
+  display: flex;
+  align-items: center;
 `;
 
 export const BtnWrap = styled.div`
-  width: 375px;
+  /* width: 375px; */
   display: flex;
   /* padding: 20px 0px; */
   justify-content: space-between;
@@ -31,15 +58,103 @@ export const Avatar = styled.video`
   width: 25rem;
   height: 25rem;
   z-index: 999;
-  margin-left: -5rem;
-  margin-top: 2rem;
+  @media screen and (max-width: 1170px) {
+    display: none;
+  }
+  /* margin-left: -5rem; */
+  /* margin-top: 2rem; */
 `;
 
 export const Content = styled.div`
-  width: calc(var(--vw, 1vw) * 100);
+  /* width: calc(var(--vw, 1vw) * 100); */
   /* padding: 0rem 10rem; */
+  font: var(--font-paragraph-intermediate) Gowun Dodum;
+  color: white;
+  /* height: calc(var(--vh, 1vh) * 70); */
 `;
 
 export const OneLine = styled.div`
   display: flex;
+`;
+
+export const ContentP = styled.p`
+  padding: 0.5rem 0rem;
+`;
+
+export const ContentSpan = styled.span`
+  color: #ffb700d8;
+  font: var(--font-label-intermediate) Gowun Dodum;
+`;
+
+export const Circle3 = styled.img`
+  width: 75rem;
+  position: absolute;
+  transform: scaleX(-1);
+  left: -30rem;
+  opacity: 50%;
+
+  @media screen and (max-width: 1023px) {
+    width: 72.5rem;
+    padding-top: 5rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 70rem;
+    padding-top: 10rem;
+  }
+`;
+
+export const IntroAllWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media screen and (max-width: 1170px) {
+    flex-direction: row;
+  }
+`;
+
+export const IntroWrap = styled.div`
+  width: 100%;
+  display: flex;
+  @media screen and (max-width: 1170px) {
+    flex-direction: column;
+    margin: 0.5rem;
+  }
+`;
+
+export const OneIntroWrap = styled.div`
+  width: 100%;
+  display: flex;
+  height: 6rem;
+  background-color: rgba(143, 194, 255, 0.25);
+  margin: 1rem 1rem 0rem 1rem;
+  border-radius: 1rem;
+  align-items: center;
+  &:hover {
+    filter: brightness(0.9);
+  }
+  @media screen and (max-width: 1170px) {
+    margin: 0.5rem;
+  }
+`;
+
+export const IntroIcon = styled.div`
+  /* width: 5rem; */
+  /* height: 5rem; */
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 1.5rem;
+`;
+
+export const IntroTextWrap = styled.div`
+  color: white;
+`;
+
+export const IntroCategory = styled.div`
+  font: var(--font-label-intermediate) Raleway;
+  color: white;
+`;
+
+export const IntroContent = styled.div`
+  font: var(--font-label-medium) Raleway;
+  cursor: pointer;
 `;
